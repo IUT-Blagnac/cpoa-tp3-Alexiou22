5fb55c8d793d72c71ffc055a949d38561bb2ca23
Alexiou22 <50455391+Alexiou22@users.noreply.github.com>
2020-11-27 21:35:00 +0100

    Update PizzaPoivronStyleBrest.java

--- src/main/java/tp3/PizzaPoivronStyleBrest.java
+++ src/main/java/tp3/PizzaPoivronStyleBrest.java
@@ -4,7 +4,7 @@ public class PizzaPoivronStyleBrest extends Pizza {
 	
 	public PizzaPoivronStyleBrest() {
 		this.nom = "Pizza sauce style brest et poivrons";
-		this.garnitures.add("Parmigiano reggiano rapé");
+		this.garnitures.add("Parmigiano reggiano rape");
 	}
 	
 	@Override
