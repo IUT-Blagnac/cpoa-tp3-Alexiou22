7a3ea4086c602b5278bfa12ebc23039e72543168
Alexiou22 <50455391+Alexiou22@users.noreply.github.com>
2020-11-27 21:35:29 +0100

    Update PizzaTestDrive.java

--- src/test/java/tp3/PizzaTestDrive.java
+++ src/test/java/tp3/PizzaTestDrive.java
@@ -6,9 +6,9 @@ public class PizzaTestDrive {
         Pizzeria boutiqueStrasbourg = FabriqueDePizzeria.getInstance().creer("Strasbourg");
 
         Pizza pizza = boutiqueBrest.commanderPizza("fromage");
-        System.out.println("JMB a commandé une " + pizza.getNom() + "\n");
+        System.out.println("JMB a commande une " + pizza.getNom() + "\n");
 
         pizza = boutiqueStrasbourg.commanderPizza("fromage");
-        System.out.println("JMI a commandé une " + pizza.getNom() + "\n");
+        System.out.println("JMI a commande une " + pizza.getNom() + "\n");
     }
 }
